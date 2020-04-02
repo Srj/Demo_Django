a4d2f621e482951e7c36e3e740c22150a3028127
Srj <44947896+Srj@users.noreply.github.com>
2020-04-03 00:31:07 +0600

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
+import cx_Oracle
 # Create your views here.
 
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello World. You are at the polls index")
+    dsn_tns  = cx_Oracle.makedsn('localhost','1521',service_name='ORCL')
+    conn = cx_Oracle.connect(user='hr',password='hr',dsn=dsn_tns)
+    c = conn.cursor()
+    print(c)
+    print('Success') 
+    c.execute("SELECT * from HR.COUNTRIES")   
+    out = ''
+    print(c)
+    for row in c : 
+        out +=str(row) + ' \n '
+    conn.close()
+    return HttpResponse(out,content_type="text/plain")
